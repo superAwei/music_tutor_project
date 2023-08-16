7024ca59bad1354941021daf0203a487320db647
AWEI <challengewei0625@gmail.com>
2023-08-16 22:33:43 +0800

    引用 swiper 問題

--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -285,6 +285,28 @@
   <section class="footer text-center fw-lighter py-4 fs-14">
     ＠ lELEMUSICE TUTOR COPYRIGHT 2023 ING
   </section>
+
+<!-- 測試輪播效果 -->
+<!-- Slider main container -->
+<div class="swiper">
+  <!-- Additional required wrapper -->
+  <div class="swiper-wrapper">
+    <!-- Slides -->
+    <div class="swiper-slide">Slide 1</div>
+    <div class="swiper-slide">Slide 2</div>
+    <div class="swiper-slide">Slide 3</div>
+    ...
+  </div>
+  <!-- If we need pagination -->
+  <div class="swiper-pagination"></div>
+
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+
+  <!-- If we need scrollbar -->
+  <div class="swiper-scrollbar"></div>
+</div>
 </template>
   
 <style lang="scss">
@@ -342,4 +364,25 @@
 </style>
 
 <script>
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 </script>
